8ceb6d8cd4ba6c117e59efe87a44564b308bdf62
Wyatt Enourato <wenourato@gmail.com>
2022-03-08 11:47:04 -0700

    Initial commit

---
+++ src/main/java/com/example/practicing/practicing/PracticingApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicing.practicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicing/practicing/PracticingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicing.practicing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
